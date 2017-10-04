f226b3540b753e55005598b3053c415a15910564
Alistair Kane <vplelmo@hotmail.co.uk>
2017-10-04 23:01:40 +0100

    ALmost finished find functions for both artist and album

--- db/console.rb
+++ db/console.rb
@@ -17,11 +17,11 @@ album1 = Album.new({"name" => 'The Black Album', "genre" => "Metal", "artist_id"
 album2 = Album.new({"name" => "A Kind of Magic", "genre" => "Pop", "artist_id" => artist2.id})
 album3 = Album.new({"name" => "Crappy Album", "genre" => "Crappy", "artist_id" => artist1.id})
 
-# artist1.name = "Blur"
-# album1.name = "Country House"
+artist1.name = "Blur"
+album1.name = "Country House"
 
-# artist1.update_artist
-# album1.update_album
+artist1.update_artist
+album1.update_album
 
 
 
@@ -29,8 +29,6 @@ album1.save()
 album2.save()
 
 album3.save()
-
-# artist2.delete_artist()
 
 
 
